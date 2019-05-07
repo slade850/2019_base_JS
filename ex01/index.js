d8fd35a6a6e05181e700e842e6d67ade8b8f20ec
Julien <julien.bardin81@gmail.com>
2019-05-07 14:48:59 +0200

    2eme_exo

--- ex01/index.js
+++ ex01/index.js
@@ -1,6 +1,6 @@
 var nb = 15;
 var char = "chaine de caractères";
 var bool = true;
-console.log("la variable nb es de type" + typeof(nb));
-console.log("la variable char es de type" + typeof(char));
-console.log("la variable bool es de type" + typeof(bool));
+console.log("la variable nb es de type: " + typeof(nb));
+console.log("la variable char es de type: " + typeof(char));
+console.log("la variable bool es de type: " + typeof(bool));
---
+++ ex02/index.js
@@ -0,0 +1,2 @@
+const valeurRandom = Math.random() > 0.5;
+valeurRandom == true ? console.log("C’est vrai"):console.log("C'est faux");
